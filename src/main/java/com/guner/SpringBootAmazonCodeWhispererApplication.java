3f6d209c107e40cb0b1002f6317ae46b6a7cbd73
Ramazan Guner <ramazan.guner@haud.com>
2024-03-18 10:47:13 +0300

    first commit

---
+++ src/main/java/com/guner/SpringBootAmazonCodeWhispererApplication.java
@@ -0,0 +1,13 @@
+package com.guner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAmazonCodeWhispererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAmazonCodeWhispererApplication.class, args);
+	}
+
+}
